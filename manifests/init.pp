e6005ad8c6de285b9d45be732a2b98e31cd18444
RamAdhikari <im_ram@hotmail.com>
2022-01-18 16:12:07 +1100

    Added init.pp and update code to install Git in Windows

---
+++ manifests/init.pp
@@ -0,0 +1,13 @@
+class install_app_windows {
+  file {'c:/temp/':
+    ensure =>directory,
+}
+  file {'c:/temp/Git-2.34.1-64-bit.exe':
+    source =>"https://192.168.0.195/iso/Git-2.34.1-64-bit.exe"
+}
+  package { 'Git':
+    ensure =>installed,
+    source =>'c:\temp/Git-2.34.1-64-bit.exe',
+    install_options =>['/VERYSILENT']
+}
+}
