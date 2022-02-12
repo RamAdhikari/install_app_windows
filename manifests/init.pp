57c3e6a95b626fe33f51563c99756a68a0fb0a42
RamAdhikari <im_ram@hotmail.com>
2022-02-12 19:40:56 +1100

    added splunk package

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,4 +10,12 @@ class install_app_windows {
     source =>'c:\temp/Git-2.34.1-64-bit.exe',
     install_options =>['/VERYSILENT']
 }
+  file{'c:/temp/splunkforwarder-8.2.4-87e2dda940d1-x64-release.msi':
+  source =>"http://192.168.0.195/iso/splunkforwarder-8.2.4-87e2dda940d1-x64-release.msi"
+}
+  package { 'Splunk':
+    ensure =>installed,
+    source =>'c:\temp/splunkforwarder-8.2.4-87e2dda940d1-x64-release.msi',
+    install_options =>['/l*v C:\TEMP\SplunkInstall.log']
+
 }
