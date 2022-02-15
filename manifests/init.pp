dc53926dec9bc188e022f351bed809f265e286aa
RamAdhikari <im_ram@hotmail.com>
2022-02-15 23:06:48 +1100

    remove splunk

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,12 +10,4 @@ class install_app_windows {
     source =>'c:\temp/Git-2.34.1-64-bit.exe',
     install_options =>['/VERYSILENT']
 }
-  file{'c:/temp/splunkforwarder-8.2.4-87e2dda940d1-x64-release.msi':
-  source =>"http://192.168.0.195/iso/splunkforwarder-8.2.4-87e2dda940d1-x64-release.msi"
-}
-  package { 'Splunk':
-    ensure =>'8.2.4.0',
-    source =>'c:\temp/splunkforwarder-8.2.4-87e2dda940d1-x64-release.msi',
-    install_options=>['AGREETOLICENSE=Yes LAUNCHSPLUNK=0 /quiet']
-}
 }
